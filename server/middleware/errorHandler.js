3b2858b96ece53c47a77798f4485da2da027b0ae
Jonathan Harris <jonathaanharris@gmail.com>
2022-12-07 21:00:03 +0700

    revised some minor problem

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -6,7 +6,10 @@ const errorHandler = (err, req, res, next) => {
     res.status(400).json({ message: 'email must be unique!' })
   } else if (err.code === 401 || err.code === 403 || err.code === 404) {
     res.status(err.code).json({ message: err.message })
-  } else {
+  } else if (err.name === "TokenExpiredError") {
+    res.status(401).json({ message: "token expired" })
+  }
+  else {
     res.status(500).json({ message: 'internal error server' })
   }
 }
